e12d714a07c7850d2f42ec7df1317c311347cdf9
youssefelganaini <youssef.elganaini@tum.de>
2023-12-28 13:11:54 +0400

    lots of changes

--- Projects/frontend-hn/src/components/Sidenav.tsx
+++ Projects/frontend-hn/src/components/Sidenav.tsx
@@ -5,7 +5,7 @@ import { IoIosHelpCircleOutline } from "react-icons/io";
 export default function Sidenav() {
   return (
     <>
-      <div className="col-auto col-sm-2 bg-dark d-flex flex-column align-items-stretch min-vh-100 text-white px-2">
+      <div className="col-auto col-sm-2 bg-dark d-flex flex-column align-items-stretch vh-100 text-white px-2">
         <button
           type="button"
           className="btn text-decoration-none text-white d-flex align-items-center my-2 fs-6"
